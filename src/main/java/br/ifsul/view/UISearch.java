4ea69a7a7ba6215e85510452069ebf4d23ef169b
erik <morbacherik@gmail.com>
2022-03-06 14:56:41 -0300

    ideia de implementação

--- src/main/java/br/ifsul/view/UISearch.java
+++ src/main/java/br/ifsul/view/UISearch.java
@@ -4,32 +4,36 @@ import br.ifsul.controller.Controller;
 import br.ifsul.model.Word;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UISearch extends JFrame {
     private JTextField searchTermField;
     private JPanel panel1;
-    private JList<String> wordListComponent;
+    private JList<Word> wordListComponent;
     private JButton searchButton;
 
-    private List<String> wordList;
+    private List<Word> wordList;
 
     public UISearch(Controller controller){
         this.setTitle("Procurar");
         this.setSize(800,400);
         this.add(this.panel1);
-//        this.wordList = new ArrayList<>();
+        this.wordList = new ArrayList<>();
 
         this.searchButton.addActionListener(e -> {
             String searchTerm = searchTermField.getText();
-            List<Word> wordResults = controller.searchWords(searchTerm);
-            System.out.println(wordResults);
-            DefaultListModel<String> model = new DefaultListModel<>();
-            if(wordResults.size() == 0)
-                model.addElement("Nenhum resultado encontrado");
-            for(Word word: wordResults)
-                model.addElement(word.toString());
+            wordList = controller.searchWords(searchTerm);
+            System.out.println(wordList);
+            DefaultListModel<Word> model = new DefaultListModel<>();
+            for(Word word: wordList)
+                model.addElement(word);
             wordListComponent.setModel(model);
+
+        });
+        wordListComponent.addListSelectionListener(e ->{
+            // Não ta funcionando direito, mas da pra utilizar coisas nesse estilo
+            System.out.println(wordList.get(e.getFirstIndex()));
         });
     }
 }
